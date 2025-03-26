f110157a01cbf0419ef2d55f1eaf8695a0c548bf
Guy <Guy@Anas-MacBook-Pro.local>
2025-03-26 09:49:02 +0100

    changed location of random number generator from views.py to dice_roller.html, and made it in JS. This means that the buttons now generate a new random number, and I'll be able to dynamically add more numbers. The next step is to add totals, highest and lowest, and perhaps to put it in a separate .js file instead of within the html itself

--- dice/views.py
+++ dice/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from random import randrange
 
 def home_page_view(request):
     return render(request, "home.html")
 
 def dice_roller_view(request):
-    context = {
-        "d4" : randrange(4)+1, 
-        "d6" : randrange(6)+1,
-        "d8" : randrange(8)+1, 
-        "d10" : randrange(10)+1, 
-        "d12" : randrange(12)+1, 
-        "d20" : randrange(20)+1, 
-        "d100" : randrange(100)+1
-        }
-    return render(request, "dice/dice_roller.html", context)
+    return render(request, "dice/dice_roller.html")
