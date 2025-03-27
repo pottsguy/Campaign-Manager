58f234297757becf65f1877c9bf37eaa22b3e9c8
Guy Potts <pottsguy@gmail.com>
2025-03-27 18:00:55 +0100

    Updated the Campaign Diary section so that new campaign posts can be added, and individual posts can be seen in full.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page_view, campaign_diary_view
+from .views import home_page_view, campaign_diary_view, individual_post_view
 
 urlpatterns = [
     path('', home_page_view, name='Home Page'),
-    path('blog/', campaign_diary_view, name='Campaign Diary')
+    path('blog/', campaign_diary_view, name='Campaign Diary'),
+    path('blog/post/<int:pk>', individual_post_view, name='Individual Post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from.models import Post
 
 def home_page_view(request):
     return render(request, "home.html")
 
 def campaign_diary_view(request):
-    return render(request, "blog/campaign_diary.html")
+    posts = Post.objects.all()
+    return render(request, "blog/campaign_diary.html", {'posts': posts})
+
+def individual_post_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "blog/individual_post.html", {'post':post})
