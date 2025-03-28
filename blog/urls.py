db152eea80913e0664704bb04fd3d8f659d7b43b
Guy Potts <pottsguy@gmail.com>
2025-03-28 12:31:23 +0100

    Added a function so that the user can now upload their own session summaries just by clicking the 'Upload a Session Summary' button and filling in the form

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home_page_view, campaign_diary_view, individual_post_view
+from .views import home_page_view, CampaignDiaryView, IndividualPostView, UploadReportView #campaign_diary_view, individual_post_view
 
 urlpatterns = [
     path('', home_page_view, name='Home Page'),
-    path('blog/', campaign_diary_view, name='Campaign Diary'),
-    path('blog/post/<int:pk>', individual_post_view, name='Individual Post')
+    path("blog/", CampaignDiaryView.as_view(), name="Campaign Diary"),
+    # path('blog/', campaign_diary_view, name='Campaign Diary'),
+    path("blog/post/<int:pk>/", IndividualPostView.as_view(), name='Individual Post'),
+    # path('blog/post/<int:pk>', individual_post_view, name='Individual Post')
+    path("blog/new/", UploadReportView.as_view(), name="Upload Session Report")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from.models import Post
 
 def home_page_view(request):
     return render(request, "home.html")
 
-def campaign_diary_view(request):
-    posts = Post.objects.all()
-    return render(request, "blog/campaign_diary.html", {'posts': posts})
+class CampaignDiaryView(ListView):
+    model = Post
+    template_name = "campaign_diary.html"
 
-def individual_post_view(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, "blog/individual_post.html", {'post':post})
+# def campaign_diary_view(request):
+#     posts = Post.objects.all()
+#     return render(request, "blog/campaign_diary.html", {'posts': posts})
+
+class IndividualPostView(DetailView):
+    model = Post
+    template_name = "individual_post.html"
+
+# def individual_post_view(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, "blog/individual_post.html", {'post':post})
+
+class UploadReportView(CreateView):
+    model = Post
+    template_name = "session_report.html"
+    fields = ['title', 'author', 'campaign', 'session', 'system', 'text']
