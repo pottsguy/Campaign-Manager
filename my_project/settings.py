d92f7bab81d126a3f43e49c63cfad2dbe324f606
Guy Potts <pottsguy@gmail.com>
2025-04-01 14:00:42 +0200

    added some basic CSS

--- my_project/settings.py
+++ my_project/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'Home Page'
