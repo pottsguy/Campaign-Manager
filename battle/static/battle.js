f30b00545ba6911112ea8ce11fcc16bdbf6f2791
Guy Potts <pottsguy@gmail.com>
2025-04-03 19:31:54 +0200

    added a list to the battle page which allows the user to add new combatants, and have them displayed automatically

--- battle/static/battle.js
+++ battle/static/battle.js
@@ -1,3 +1,8 @@
-function hello_world() {
-    alert("Hello World");
+const names = []
+
+function updateNameDisplay() {
+    const li = document.createElement("li");
+    const newName = document.createTextNode(document.getElementById('nameInput').value);
+    li.appendChild(newName);
+    document.getElementById("nameList").appendChild(li);
 }
--- battle/views.py
+++ battle/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def battle_view(request) :
     return render(request, 'battle.html')
