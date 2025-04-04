603ca52a8594aa71e3b9aae00910aa01a68e24f1
Guy Potts <pottsguy@gmail.com>
2025-04-04 12:21:46 +0200

    added notes and delete button to battle page entries. Next would be to add different zones and list reorderability

--- battle/static/battle.js
+++ battle/static/battle.js
@@ -1,8 +1,22 @@
 const names = []
 
-function updateNameDisplay() {
-    const li = document.createElement("li");
-    const newName = document.createTextNode(document.getElementById('nameInput').value);
+function addName() {
+    const li = document.createElement("li"); // returns a https://developer.mozilla.org/en-US/docs/Web/API/HTMLLIElement
+    const del = document.createTextNode("Delete");
+    const btn = document.createElement('button'); // returns a https://developer.mozilla.org/en-US/docs/Web/API/HTMLButtonElement
+    btn.addEventListener("click", removeCombatant);
+    const notes = document.createElement('input'); // returns a https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
+    const newName = document.createTextNode(document.getElementById('nameInput').value); // returns a https://developer.mozilla.org/en-US/docs/Web/API/Text
+    notes.value = document.getElementById('creationNotes').value;
+    notes.style.width = '450px';
+    // const creationNotes = document.createTextNode(document.getElementById('creationNotes').value);
     li.appendChild(newName);
     document.getElementById("nameList").appendChild(li);
+    li.append(notes);
+    li.appendChild(btn);
+    btn.appendChild(del);
+
+    function removeCombatant() {
+        li.remove()
+    }
 }
