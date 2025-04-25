485897bcd81f72cfcdbd783eb6d65405b258020a
Guy Potts <pottsguy@gmail.com>
2025-04-25 17:32:45 +0200

    Made it so that the list of combatants is reorderable

--- battle/static/battle.js
+++ battle/static/battle.js
@@ -2,14 +2,18 @@ const names = []
 
 function addName() {
     const li = document.createElement("li"); // returns a https://developer.mozilla.org/en-US/docs/Web/API/HTMLLIElement
+    li.setAttribute('draggable', 'true');
+    li.setAttribute('ondragend', 'dragEnd()');
+    li.setAttribute('ondragover', 'dragOver(event)');
+    li.setAttribute('ondragstart', 'dragStart(event)');
     const del = document.createTextNode("Delete");
     const btn = document.createElement('button'); // returns a https://developer.mozilla.org/en-US/docs/Web/API/HTMLButtonElement
     btn.addEventListener("click", removeCombatant);
     const notes = document.createElement('input'); // returns a https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
     const newName = document.createTextNode(document.getElementById('nameInput').value); // returns a https://developer.mozilla.org/en-US/docs/Web/API/Text
     notes.value = document.getElementById('creationNotes').value;
-    notes.style.width = '400px';
-    // const creationNotes = document.createTextNode(document.getElementById('creationNotes').value);
+    notes.style.width = '200px';
+    const creationNotes = document.createTextNode(document.getElementById('creationNotes').value);
     li.appendChild(newName);
     document.getElementById("nameList").appendChild(li);
     li.append(notes);
