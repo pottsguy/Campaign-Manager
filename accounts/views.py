c76ec8123f6618b76b4af1ab187d1305a80da811
Guy Potts <pottsguy@gmail.com>
2025-05-12 15:25:33 +0200

    the campaign page now shows a list of the campaigns that the user is a part of

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from blog.models import Campaign
 
 class SignUpView(CreateView) :
     form_class = UserCreationForm
@@ -9,4 +10,5 @@ class SignUpView(CreateView) :
     template_name = "registration/sign_up.html"
 
 def CampaignsView(request) :
-    return render(request, 'campaigns.html')
+    campaign_list = Campaign.objects.filter(users=request.user)
+    return render(request, 'campaigns.html', {'campaign_list':campaign_list})
